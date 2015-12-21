17c5f9c13274f5b23a6678f9c86c8d410ac0b030
Dominique Feyer <dfeyer@ttree.ch>
2015-12-21 21:57:32 +0100

    TASK: Change RenderingController::showAction method signature

--- Classes/Controller/RenderingController.php
+++ Classes/Controller/RenderingController.php
@@ -5,6 +5,7 @@ use Ttree\OutOfBandRendering\Factory\PresetDefinitionFactory;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Exception;
 use TYPO3\Flow\Mvc\Controller\ActionController;
+use TYPO3\Flow\Property\PropertyMapper;
 use TYPO3\Form\Exception\PresetNotFoundException;
 use TYPO3\Neos\Controller\Exception\NodeNotFoundException;
 use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
@@ -26,6 +27,12 @@ class RenderingController extends ActionController {
     protected $defaultViewObjectName = 'TYPO3\Neos\View\TypoScriptView';
 
     /**
+     * @var PropertyMapper
+     * @Flow\Inject
+     */
+    protected $propertyMapper;
+
+    /**
      * @Flow\Inject
      * @var PresetDefinitionFactory
      */
@@ -38,17 +45,18 @@ class RenderingController extends ActionController {
     protected $presets;
 
     /**
-     * @param NodeInterface $node
-     * @param string $preset
+     * @param array $rendering
      * @throws Exception
      */
-    public function showAction(NodeInterface $node = NULL, $preset) {
-        if ($node === NULL) {
+    public function showAction($rendering) {
+        /** @var NodeInterface $renderingNode */
+        $renderingNode = $this->propertyMapper->convert($rendering['node'], 'TYPO3\TYPO3CR\Domain\Model\NodeInterface');
+        if ($renderingNode === NULL) {
             throw new NodeNotFoundException('The requested node does not exist or isn\'t accessible to the current user', 1442327533);
         }
-        $this->view->assign('value', $node);
-        $presetDefinition = $this->presetDefinitionFactory->create($preset, $node);
-        $this->view->setTypoScriptPath($presetDefinition->getTypoScriptPath($node));
+        $this->view->assign('value', $renderingNode);
+        $presetDefinition = $this->presetDefinitionFactory->create($rendering['preset'], $renderingNode);
+        $this->view->setTypoScriptPath($presetDefinition->getTypoScriptPath($renderingNode));
     }
 
 }

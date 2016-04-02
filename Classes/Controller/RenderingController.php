11582b4d3a5af30964dec00d97760d64e8276325
Dominique Feyer <dfeyer@ttree.ch>
2016-04-02 21:16:15 +0200

    TASK: Code cleanup in the rendering controller

--- Classes/Controller/RenderingController.php
+++ Classes/Controller/RenderingController.php
@@ -6,7 +6,6 @@ use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Exception;
 use TYPO3\Flow\Mvc\Controller\ActionController;
 use TYPO3\Flow\Property\PropertyMapper;
-use TYPO3\Form\Exception\PresetNotFoundException;
 use TYPO3\Neos\Controller\Exception\NodeNotFoundException;
 use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 use TYPO3\TypoScript\View\TypoScriptView;
@@ -39,24 +38,19 @@ class RenderingController extends ActionController {
     protected $presetDefinitionFactory;
 
     /**
-     * @Flow\InjectConfiguration("presets")
-     * @var array
-     */
-    protected $presets;
-
-    /**
-     * @param array $rendering
+     * @param string $node
+     * @param string $preset
      * @throws Exception
      */
-    public function showAction($rendering) {
-        /** @var NodeInterface $renderingNode */
-        $renderingNode = $this->propertyMapper->convert($rendering['node'], 'TYPO3\TYPO3CR\Domain\Model\NodeInterface');
-        if ($renderingNode === NULL) {
+    public function showAction($node, $preset) {
+        /** @var NodeInterface $node */
+        $node = $this->propertyMapper->convert($node, 'TYPO3\TYPO3CR\Domain\Model\NodeInterface');
+        if ($node === NULL) {
             throw new NodeNotFoundException('The requested node does not exist or isn\'t accessible to the current user', 1442327533);
         }
-        $this->view->assign('value', $renderingNode);
-        $presetDefinition = $this->presetDefinitionFactory->create($rendering['preset'], $renderingNode);
-        $this->view->setTypoScriptPath($presetDefinition->getTypoScriptPath($renderingNode));
+        $this->view->assign('value', $node);
+        $presetDefinition = $this->presetDefinitionFactory->create($preset, $node);
+        $this->view->setTypoScriptPath($presetDefinition->getTypoScriptPath($node));
     }
 
 }
